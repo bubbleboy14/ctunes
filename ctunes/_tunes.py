e871ce93d9826808548204afa8602f120f21a00e
bubbleboy14 <mario.balibrera@gmail.com>
2016-08-23 17:04:53 -0700

    /_tunes handler sorts album names before returning them

--- ctunes/_tunes.py
+++ ctunes/_tunes.py
@@ -4,7 +4,9 @@ from cantools.web import respond, succeed
 def response():
     dirs = {}
     def scan(nothing, dirname, fnames):
-        dirs[os.path.split(dirname)[-1]] = [f for f in fnames if f != "mp3.zip"]
+        fz = [f for f in fnames if f != "mp3.zip"]
+        fz.sort()
+        dirs[os.path.split(dirname)[-1]] = fz
     os.path.walk("mp3", scan, None)
     succeed(dirs)
 
